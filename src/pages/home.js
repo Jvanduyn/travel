0fbc578ae534d0d41feb5789fbfe239587f675c5
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-09-26 20:12:41 -0400

    initial commit for travel app

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Home() {
+    return (
+        <main>
+            <div className="bg-auto">
+                <div className="w-full">
+                    <Login />
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Home;
